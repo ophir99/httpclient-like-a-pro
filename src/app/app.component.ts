10cf0980b3900ce8e995cebffcc0f98736e27b10
kavya.pl <kavya.pl@gmail.com>
2019-01-17 10:46:33 +0530

    Simple commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { FormBuilder, FormGroup, FormArray } from "@angular/forms";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'httpclient-like-a-pro';
+  title = "httpclient-like-a-pro";
+  postForm: FormGroup;
+  posts = [];
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+    this.createPostForm();
+    this.getAll();
+  }
+
+  createPostForm() {
+    this.postForm = this.formBuilder.group({
+      title: [],
+      desc: [],
+      hashtags: this.formBuilder.array([])
+    });
+  }
+
+  addHashtags() {
+    (this.postForm.get("hashtags") as FormArray).push(
+      this.formBuilder.control("")
+    );
+  }
+  getAll() {
+    this.http.get("http://localhost:8900/posts/all").subscribe(
+      (res: any) => {
+        console.log(res);
+        this.posts = res.response;
+      },
+      err => {
+        console.log("Error", err);
+      }
+    );
+  }
+  createPost() {
+    this.http
+      .post("http://localhost:8900/posts/new", this.postForm.value)
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log("Error", err);
+        }
+      );
+  }
 }
